2303f56104fac01d85d700ca83895285de0f653f
cherry951010 <13037521553@163.com>
2020-07-10 15:44:11 +0800

    添加读取json数据读取测试用例运行

--- testCases/test_expection/test_topic.py
+++ testCases/test_expection/test_topic.py
@@ -2,20 +2,24 @@
 测试 发布话题的异常场景
 
 """
+import json
+
 import  requests
 import  pytest
 defaulturl = 'http://49.233.108.117:3000/api/v1'
 accesstoken = 'ebb04940-7910-4f74-a175-fe5dd01cb0f9'
-testdata = [
-    ({'accesstoken':'1','title':'','tab':'','content':''},401,'错误的accessToken'),
-    ({'accesstoken':accesstoken,'title':'1111111','tab':'','content':''},400,'必须选择一个版块'),
-    ({'accesstoken':accesstoken,'title':'','tab':'','content':''},400,'标题不能为空'),
-    ({'accesstoken':accesstoken,'title':'1','tab':'','content':''},400,'标题字数太多或太少'),
-    ({'accesstoken':accesstoken,'title':'hellowoed','tab':'ask','content':''},400,'内容不可为空')
-]
+# testdata = [
+#     ({'accesstoken':'1','title':'','tab':'','content':''},401,'错误的accessToken'),
+#     ({'accesstoken':accesstoken,'title':'1111111','tab':'','content':''},400,'必须选择一个版块'),
+#     ({'accesstoken':accesstoken,'title':'','tab':'','content':''},400,'标题不能为空'),
+#     ({'accesstoken':accesstoken,'title':'1','tab':'','content':''},400,'标题字数太多或太少'),
+#     ({'accesstoken':accesstoken,'title':'hellowoed','tab':'ask','content':''},400,'内容不可为空')
+# ]
 
-
+data = json.load(open('data/topics.json',mode='r',encoding='utf8'))
+testdata = data["test_create_topics"]
 @pytest.mark.parametrize('topic_data,code,errormsg',testdata)
+
 
 def test_create_topic(topic_data,code,errormsg):
     url = defaulturl + '/topics'
---
+++ tmp.py
@@ -0,0 +1,18 @@
+import  json
+
+#json 编码
+test = ['foo',{'bar':('baz',None,1.0,2)}]
+data = json.dumps(test)
+print(data,type(data))
+#json 解码
+test_str = '{"usernam":"xiaomin"}'
+data = json.load(test_str)
+print(data,type(data))
+#加载json文件转换Python格式load
+data = json.load(['foo',{'bar':('baz',None,1.0,2)}])
+json.dump(data,open('data/tmp.json',mode='r',encoding='utf8'))
+print(data,type(data))
+#写入文件
+data = json.load(['foo',{'bar':('baz',None,1.0,2)}])
+json.dump(data,open('data/tmp.json',mode='w',encoding='utf8'))
+
